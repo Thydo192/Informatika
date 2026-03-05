62a25569afab608aa33c6d040aaa17d8e721f047
Thydo192 <1q2w3e4r5t6y366@gmail.com>
2026-03-05 11:39:32 +0300

    Delete Лабораторная работа 1 task3.py

--- Лабораторная работа 1 task3.py
+++
@@ -1,9 +0,0 @@
-list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
-# найдем середину
-F = len(list_players) // 2
-# раздели команду на две равные
-team_A = list_players[:F]
-team_B = list_players[F:]
-# выведем список учатсников каждой команды
-print(team_A)
-print(team_B)
